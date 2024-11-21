37121eb92b20cdd7a7ecf869d176a85020c0d93d
Akankshap24 <147901332+Akankshap24@users.noreply.github.com>
2024-11-21 16:03:35 +0530

    changes

---
+++ GFG/frequency.py
@@ -0,0 +1,7 @@
+class Solution:
+    def findFrequency(self, arr, x):
+        count = 0
+        for element in arr:
+            if element == x:
+                count += 1
+        return count 
