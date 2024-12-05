4d671dac33e70fd39488227a367581515d8f986c
Akankshap24 <147901332+Akankshap24@users.noreply.github.com>
2024-12-05 23:24:21 +0530

    code

---
+++ GFG/pairSum.py
@@ -0,0 +1,20 @@
+class Solution:
+    def countPair (self, k, arr) : 
+        left = 0
+        right = len(arr) - 1
+        count = 0
+        
+        while left < right:
+            currSum = arr[left] + arr[right]
+            if currSum == k:
+                count += 1
+                left += 1
+                right -= 1
+            
+            elif currSum < k:
+                left += 1
+                
+            else:
+                right -= 1
+            
+        return count
