a5143910a7c76400d47d42966737bd81ed61ee8a
Akankshap24 <147901332+Akankshap24@users.noreply.github.com>
2024-12-11 23:25:26 +0530

    Contest

---
+++ CodeChef/Contests/Starters 134/Money_double.py
@@ -0,0 +1,15 @@
+def money_double(x, y):
+    for i in range(y):
+        if x * 2 >= x + 1000:
+            x *= 2
+        else:
+            x += 1000
+    return x  
+
+T = int(input())
+
+for i in range(T):
+    x, y = input().split()
+    x = int(x)
+    y = int(y)
+    
--- CodeChef/Contests/Starters 134/Morning_run.py
+++ CodeChef/Contests/Starters 134/Morning_run.py
@@ -1,15 +1,8 @@
-def money_double(x, y):
-    for i in range(y):
-        if x * 2 >= x + 1000:
-            x *= 2
-        else:
-            x += 1000
-    return x  
-
-T = int(input())
-
-for i in range(T):
-    x, y = input().split()
-    x = int(x)
-    y = int(y)
-    
+X, Y = input().split()
+X = int(X)
+Y = int(Y)
+perimeter = 2 * (X + Y)
+if perimeter >= 1000:
+    print("YES")
+else:
+    print("NO")
