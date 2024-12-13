5113add81e101ba6a2922f8afc81a8db94ab5a42
Akankshap24 <147901332+Akankshap24@users.noreply.github.com>
2024-12-13 22:47:23 +0530

    Contest

---
+++ CodeChef/Contests/Starters 140/yoga_class.py
@@ -0,0 +1,11 @@
+T = int(input())
+
+for _ in range(T):
+    N, X, Y = map(int, input().split())
+    max_earnings = 0
+
+    for k in range((N // 2) + 1):
+        earnings = k * Y + (N - 2 * k) * X
+        if earnings > max_earnings:
+            max_earnings = earnings
+    print(max_earnings)
