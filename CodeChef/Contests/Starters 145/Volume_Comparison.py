04508a8dfc0aabed01d5fbed676dc52643074e04
Akankshap24 <147901332+Akankshap24@users.noreply.github.com>
2024-12-14 23:05:54 +0530

    Contest

---
+++ CodeChef/Contests/Starters 145/Volume_Comparison.py
@@ -0,0 +1,10 @@
+def volume(A, B, C, X):
+    v_cuboid = A * B * C
+    v_cube = X ** 3
+    
+    if v_cuboid > v_cube:
+        print("Cuboid")
+    elif v_cuboid < v_cube:
+        print("Cube")
+    else:
+        print("Equal")
