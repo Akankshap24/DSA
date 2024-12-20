ccf3fb7a94517bab8de51e652fc717dec545574f
Akankshap24 <147901332+Akankshap24@users.noreply.github.com>
2024-12-20 13:54:47 +0530

    Practice

---
+++ CodeChef/Basic Maths/Fitness.py
@@ -0,0 +1,8 @@
+# Chef travels to and from his office, which is X km away, 5 days a week. Find the total distance he travels in a week.
+
+t = int(input())
+while t > 0:
+    x = int(input())
+    t -= 1
+    total_distance = 10 * x
+    print(total_distance)
