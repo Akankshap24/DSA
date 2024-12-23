2883df6a04e40d6d01ec0f2ab70606ba6887d9a5
Akankshap24 <147901332+Akankshap24@users.noreply.github.com>
2024-12-23 21:28:00 +0530

    Practice

---
+++ CodeChef/Basic Maths/Candy_store.py
@@ -0,0 +1,12 @@
+# Store: Calculate total earnings based on chocolates sold and Chef's daily goal
+
+t = int(input())
+
+while t > 0:
+    x, y = map(int, input().split())
+    if y <= x:
+        total_amount = y
+    else:
+        total_amount = x + 2 * (y - x)
+    print(total_amount)
+    t -= 1
