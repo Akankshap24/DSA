fb1d8b19defecc9d464d6daa191c3eef8b381b29
Akankshap24 <147901332+Akankshap24@users.noreply.github.com>
2025-01-20 22:58:14 +0530

    Code

--- Class Practice Questions/treemap.py
+++ Class Practice Questions/treemap.py
@@ -16,3 +16,16 @@ print(value) # Output = 3
 tree_map.pop('b')
 print(tree_map) # Output = SortedDict({'a': 1})
 
+# Iterating through the map
+# Key
+for key in tree_map.keys():
+    print(key) # Output = a 
+
+#Value
+for value in tree_map.values():
+    print(value) # Output = 1 
+
+#KeyValuePair
+for key,value in tree_map.items():
+    print(key,value) # Output = a 1 
+  
