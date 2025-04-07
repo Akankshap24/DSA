ba8f9eaf5274ab5eb08504df262876327a9c7961
Akankshap24 <147901332+Akankshap24@users.noreply.github.com>
2025-04-07 22:14:00 +0530

    Code

---
+++ Leetcode/max_subarray.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxSubArray(self, nums):
+        curr_sum = 0
+        max_sum = float('-inf')
+
+        for val in nums:
+            curr_sum += val
+            max_sum = max(curr_sum, max_sum)
+
+            if curr_sum < 0:
+                curr_sum = 0
+
+        return max_sum
