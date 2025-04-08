d1c8fedbd2b9f692b49783e1f9da53f9b1d44c1c
Akankshap24 <147901332+Akankshap24@users.noreply.github.com>
2025-04-08 21:51:30 +0530

    Code

---
+++ Leetcode/design_hashmap.py
@@ -0,0 +1,35 @@
+class MyHashMap:
+
+    def __init__(self):
+        self.capacity = 1000  # Initial capacity
+        self.data = [[] for _ in range(self.capacity)]
+
+    def _hash(self, key: int) -> int:
+        return key % self.capacity
+
+    def put(self, key: int, value: int) -> None:
+        index = self._hash(key)
+        for i in range(len(self.data[index])):
+            k, v = self.data[index][i]
+            if k == key:
+                self.data[index][i] = (key, value)
+                return
+        self.data[index].append((key, value))
+
+    def get(self, key: int) -> int:
+        index = self._hash(key)
+        for k, v in self.data[index]:
+            if k == key:
+                return v
+        return -1
+
+    def remove(self, key: int) -> None:
+        index = self._hash(key)
+        for i in range(len(self.data[index])):
+            k, v = self.data[index][i]
+            if k == key:
+                self.data[index].pop(i)
+                return
+
+        
+
