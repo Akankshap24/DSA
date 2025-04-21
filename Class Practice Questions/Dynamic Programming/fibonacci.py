69a0d2c7bc71acc041ee57f5e75a63db4dee8cae
Akankshap24 <147901332+Akankshap24@users.noreply.github.com>
2025-04-21 23:40:21 +0530

    Changes

--- Class Practice Questions/Dynamic Programming/fibonacci.py
+++ Class Practice Questions/Dynamic Programming/fibonacci.py
@@ -2,13 +2,13 @@ class Solution:
     def fib(self, n: int) -> int:
         memo = {}
         
-        def fibonacci(n):
+        def fibon(n):
             if n <= 1:
                 return n
             if n not in memo:
-                memo[n] = fibonacci(n-1) + fibonacci(n-2)
+                memo[n] = fibon(n-1) + fibon(n-2)
             return memo[n]
         
-        return fibonacci(n)
+        return fibon(n)
 
     
