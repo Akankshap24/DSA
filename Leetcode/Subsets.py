9624180393daf18b6bddeb1baf1645420fae0557
Akankshap24 <147901332+Akankshap24@users.noreply.github.com>
2025-07-18 23:03:17 +0530

    Code

--- Leetcode/Subsets.py
+++ Leetcode/Subsets.py
@@ -1,13 +1,13 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        res = []
+        result = []
 
         def dfs(index, path):
-            res.append(path)
+            result.append(path)
             for i in range(index, len(nums)):
                 dfs(i + 1, path + [nums[i]])
         
         dfs(0, [])
-        return res
+        return result
 
         
