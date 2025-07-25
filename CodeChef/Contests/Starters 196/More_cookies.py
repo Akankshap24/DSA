3665f9946a5b7492ea9758dcc1f423237e6a9afb
Akankshap24 <147901332+Akankshap24@users.noreply.github.com>
2025-07-25 21:50:43 +0530

    Code

--- CodeChef/Contests/Starters 196/More_cookies.py
+++ CodeChef/Contests/Starters 196/More_cookies.py
@@ -3,8 +3,8 @@ for _ in range(T):
     N, C = map(int, input().split())
     A = list(map(int, input().split()))
     for x in range(101): 
-        alice = C + x
-        if any(a < alice for a in A) and all(a != alice for a in A):
+        ali = C + x
+        if any(a < ali for a in A) and all(a != ali for a in A):
             print(x)
             break
 
