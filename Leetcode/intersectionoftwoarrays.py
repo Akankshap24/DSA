0aaddca8e993125f97b644c5bbcad0eb80574547
Akankshap24 <147901332+Akankshap24@users.noreply.github.com>
2025-07-30 22:39:17 +0530

    Code

--- Leetcode/intersectionoftwoarrays.py
+++ Leetcode/intersectionoftwoarrays.py
@@ -1,5 +1,5 @@
 class Solution:
-    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+    def inter(self, nums1: List[int], nums2: List[int]) -> List[int]:
         set1 = set(nums1)
         set2 = set(nums2)
 
