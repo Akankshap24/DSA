b55b201c012a6b9f1bc14a05e15205192c2f81ef
Akankshap24 <147901332+Akankshap24@users.noreply.github.com>
2025-08-04 22:01:20 +0530

    Code

---
+++ Leetcode/jump_game_2.py
@@ -0,0 +1,19 @@
+class Solution:
+    def jump(self, nums: list[int]) -> int:
+        n = len(nums)
+        if n <= 1:
+            return 0
+
+        jump = 0
+        curr_end = 0
+        far = 0
+
+        for i in range(n - 1):  
+            far = max(far, i + nums[i])
+            if i == curr_end:
+                jump += 1
+                curr_end = far
+                if curr_end >= n - 1:
+                    break
+
+        return jump
