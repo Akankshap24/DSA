b189b1c60e9a7664359414beba1d12200736b502
Akankshap24 <147901332+Akankshap24@users.noreply.github.com>
2025-08-11 23:34:25 +0530

    Code

---
+++ Leetcode/generate_parenthesis.py
@@ -0,0 +1,9 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = []
+        def back(s, o, c):
+            if o == c == n: res.append(s); return
+            if o < n: back(s+"(", o+1, c)
+            if c < o: back(s+")", o, c+1)
+        back("", 0, 0)
+        return res
