0bbe49ecb982dbbf5484242a8b1336819523aa08
Akankshap24 <147901332+Akankshap24@users.noreply.github.com>
2025-08-30 23:10:56 +0530

    Code

--- Leetcode/Pascals_triangle.py
+++ Leetcode/Pascals_triangle.py
@@ -1,10 +1,10 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        triangle = []
+        tri = []
         for i in range(numRows):
             row = [1] * (i + 1)
             for j in range(1, i):
-                row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
-            triangle.append(row)
-        return triangle
+                row[j] = tri[i - 1][j - 1] + tri[i - 1][j]
+            tri.append(row)
+        return tri
         
