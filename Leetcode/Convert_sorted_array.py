23881a3c06c2a1334693f0a65e4bd7a9008505a4
Akankshap24 <147901332+Akankshap24@users.noreply.github.com>
2025-10-13 23:07:22 +0530

    Code

--- Leetcode/Convert_sorted_array.py
+++ Leetcode/Convert_sorted_array.py
@@ -1,3 +1,5 @@
+# Definition for a binary tree node.
+# https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
 class Solution:
     def sortedArray(self, nums: List[int]) -> Optional[TreeNode]:
         if not nums: return None
