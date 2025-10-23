a293ae36304fb8885e4a1e7849be585034cd6288
Akankshap24 <147901332+Akankshap24@users.noreply.github.com>
2025-10-23 23:20:59 +0530

    Changes

--- Leetcode/zigzag.py
+++ Leetcode/zigzag.py
@@ -1,3 +1,5 @@
+# ZigZag Conversion
+# https://leetcode.com/problems/zigzag-conversion/
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
         if numRows == 1 or numRows >= len(s):
