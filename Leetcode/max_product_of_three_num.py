f632786af38076548afc54dd0bc4731722d27d2a
Akankshap24 <147901332+Akankshap24@users.noreply.github.com>
2025-11-11 21:51:47 +0530

    Changes

--- Leetcode/max_product_of_three_num.py
+++ Leetcode/max_product_of_three_num.py
@@ -1,3 +1,5 @@
+# 628. Maximum Product of Three Numbers
+# https://leetcode.com/problems/maximum-product-of-three-numbers/
 from typing import List
 class Solution:
     def maximumProduct(self, nums: List[int]) -> int:
