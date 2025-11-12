439af2e62778abb8cc309abe2dd0f9569b805d34
Akankshap24 <147901332+Akankshap24@users.noreply.github.com>
2025-11-12 22:05:19 +0530

    Changes

--- Leetcode/multiply_strings.py
+++ Leetcode/multiply_strings.py
@@ -1,3 +1,5 @@
+## 43. Multiply Strings
+##https://leetcode.com/problems/multiply-strings/
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
         if num1 == "0" or num2 == "0": return "0"
