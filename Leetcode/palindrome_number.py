d24737b4cc18eb2fabf474ab152be232ba20ef5f
Akankshap24 <147901332+Akankshap24@users.noreply.github.com>
2025-11-15 23:00:30 +0530

    Changes

--- Leetcode/palindrome_number.py
+++ Leetcode/palindrome_number.py
@@ -1,3 +1,5 @@
+## Palindrome Number
+## https://leetcode.com/problems/palindrome-number/
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         if x < 0 or (x % 10 == 0 and x != 0):
