e11698f85d7f0a3270285665f9cb72d8eb7fbac3
Akankshap24 <147901332+Akankshap24@users.noreply.github.com>
2025-11-21 23:00:19 +0530

    Changes

--- GFG/distinct_elements.py
+++ GFG/distinct_elements.py
@@ -1,3 +1,5 @@
+# Function to count distinct elements in an array
+# https://www.geeksforgeeks.org/python-program-to-count-distinct-elements-in-an-array/
 def distinct(arr):
     distinct_arr = set(arr)
     return len(distinct_arr)
