07beb70d0107ad2dc3c7ac513353534746d9dac4
Akankshap24 <147901332+Akankshap24@users.noreply.github.com>
2025-11-22 21:36:16 +0530

    Changes

--- GFG/Factorial.py
+++ GFG/Factorial.py
@@ -1,3 +1,5 @@
+# Factorial of a number
+# https://practice.geeksforgeeks.org/problems/factorial-of-a-number-1587115620/1
 class Solution:
     def factorial (self, n):
         if n == 0 or n == 1:
