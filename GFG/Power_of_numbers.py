254ef414aa85eebfb6802c863945ef1201a5bd75
Akankshap24 <147901332+Akankshap24@users.noreply.github.com>
2025-11-28 22:14:56 +0530

    Changes

--- GFG/Power_of_numbers.py
+++ GFG/Power_of_numbers.py
@@ -1,3 +1,5 @@
+# Problem: Power of Numbers
+# https://practice.geeksforgeeks.org/problems/power-of-numbers-1587115620/1
 class Solution:
     def reverse_exponentiation(self, n):
         reverse_n = int(str(n)[::-1])
