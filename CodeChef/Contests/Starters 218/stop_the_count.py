8930e6e83ea65a1a981cf08e3f4c2b7d3c92df8d
Akankshap24 <147901332+Akankshap24@users.noreply.github.com>
2026-01-04 21:32:32 +0530

    Contest

---
+++ CodeChef/Contests/Starters 218/stop_the_count.py
@@ -0,0 +1,14 @@
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    S = input()
+    ones = ans = 0
+
+    for i in range(N):
+        if S[i] == '1':
+            ones += 1
+        if ones > (i + 1 - ones):
+            ans += 1
+
+    print(ans)
+
